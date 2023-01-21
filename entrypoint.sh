119d0ea6f3df025b8d9cf51a310ae36e658798ea
Bjoern Michaelsen <bjoern.michaelsen@libreoffice.org>
2023-01-21 01:03:24 +0100

    simplify: just build an github-pages artifact
    - there are actions to upload and deploy these
    - make the script POSIX sh and pass shellcheck

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,53 +1,31 @@
-#!/bin/bash
+#!/bin/sh
 set -e
-set -o pipefail
 
-if [[ -n "$TOKEN" ]]; then
-    GITHUB_TOKEN=$TOKEN
-fi
+#if test -n "${TOKEN}"; then
+#    GITHUB_TOKEN=${TOKEN}
+#fi
+#
+#if test -z "${GITHUB_TOKEN}"; then
+#    echo "Set the GITHUB_TOKEN env variable."
+#    exit 1
+#fi
+#
+#if test -z "${GITHUB_REPOSITORY}"; then
+#    echo "Set the GITHUB_REPOSITORY env variable."
+#    exit 1
+#fi
 
-if [[ -z "$PAGES_BRANCH" ]]; then
-    PAGES_BRANCH="gh-pages"
-fi
+echo "Starting build..."
 
-if [[ -z "$GITHUB_TOKEN" ]]; then
-    echo "Set the GITHUB_TOKEN env variable."
-    exit 1
-fi
+echo "Fetching themes"
+git config --global url."https://".insteadOf git://
+git config --global url."https://github.com/".insteadOf git@github.com:
+git submodule update --init --recursive
 
-if [[ -z "$GITHUB_REPOSITORY" ]]; then
-    echo "Set the GITHUB_REPOSITORY env variable."
-    exit 1
-fi
+version=$(zola --version)
+echo "Using ${version}"
 
-main() {
-    echo "Starting deploy..."
+zola build
 
-    echo "Fetching themes"
-    git config --global url."https://".insteadOf git://
-    git config --global url."https://github.com/".insteadOf git@github.com:
-    git submodule update --init --recursive
-
-    version=$(zola --version)
-    remote_repo="https://${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
-    remote_branch=$PAGES_BRANCH
-
-    echo "Using $version"
-
-    zola build
-
-    echo "Pushing artifacts to ${GITHUB_REPOSITORY}:$remote_branch"
-
-    cd public
-    git init
-    git config user.name "GitHub Actions"
-    git config user.email "github-actions-bot@users.noreply.github.com"
-    git add .
-
-    git commit -m "Deploy ${GITHUB_REPOSITORY} to ${GITHUB_REPOSITORY}:$remote_branch"
-    git push --force "${remote_repo}" master:${remote_branch}
-
-    echo "Deploy complete"
-}
-
-main "$@"
+#tar czf github-pages -C public
+echo "Build complete."
