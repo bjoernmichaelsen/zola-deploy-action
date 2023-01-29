33ee1f9ccfb51180c4b39f678481bac8370da9c4
Bjoern Michaelsen <bjoern.michaelsen@libreoffice.org>
2023-01-29 20:33:10 +0100

    adapt to safe.directory update

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ echo "Starting build..."
 echo "Fetching themes"
 git config --global url."https://".insteadOf git://
 git config --global url."https://github.com/".insteadOf git@github.com:
+git config --global --add safe.directory "*"
 git submodule update --init --recursive
 
 version=$(zola --version)
